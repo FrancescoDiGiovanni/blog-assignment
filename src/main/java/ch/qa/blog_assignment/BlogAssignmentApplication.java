46325b940f9371dd43f3f27231a73a05ebcfefe9
Francesco <f.digiovanni2411@gmail.com>
2024-08-06 18:14:41 +0200

    removed spring security

--- src/main/java/ch/qa/blog_assignment/BlogAssignmentApplication.java
+++ src/main/java/ch/qa/blog_assignment/BlogAssignmentApplication.java
@@ -2,9 +2,8 @@ package ch.qa.blog_assignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class BlogAssignmentApplication {
 
 	public static void main(String[] args) {
