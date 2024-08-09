d81d662f9ee376bd333be175f9cdd366d24e83f6
Francesco <f.digiovanni2411@gmail.com>
2024-08-09 09:45:38 +0200

    modified db structure

--- src/main/java/ch/qa/blog_assignment/entities/Tag.java
+++ src/main/java/ch/qa/blog_assignment/entities/Tag.java
@@ -19,7 +19,7 @@ public class Tag {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private int id;
 
-    @Column(name = "name")
+    @Column(name = "name", unique = true)
     private String name;
 
     @ManyToOne
