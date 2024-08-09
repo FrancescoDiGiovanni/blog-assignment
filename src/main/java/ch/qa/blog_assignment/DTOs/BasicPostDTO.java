d15a5b424566d5abedc3ded307ad5b3c2da7357c
Francesco <f.digiovanni2411@gmail.com>
2024-08-09 09:46:03 +0200

    created new dto for create api

---
+++ src/main/java/ch/qa/blog_assignment/DTOs/BasicPostDTO.java
@@ -0,0 +1,17 @@
+package ch.qa.blog_assignment.DTOs;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class BasicPostDTO {
+    private String title;
+    private String content;
+    private String image;
+    private AuthorDTO author;
+}
